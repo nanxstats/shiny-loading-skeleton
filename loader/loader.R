8cd49a54d0eb7e9da5c68175299f93514953873f
Nan Xiao <me@nanx.me>
2022-04-21 23:18:33 -0400

    Give loader a title

--- loader/loader.R
+++ loader/loader.R
@@ -16,28 +16,31 @@ card <- function(title, ...) {
 }
 
 useLoader <- function() {
-  loaderDiv <- div(
-    class = "loader", id = "loader",
+  loaderDiv <- htmltools::tagList(
+    htmltools::tags$head(htmltools::tags$title("shiny-loading-skeleton")),
     div(
-      class = "container-fluid loading-skeleton",
-      style = "margin-top: 76px;",
-      fluidRow(
-        column(
-          width = 10, offset = 1,
-          fluidRow(
-            column(
-              width = 4,
-              card(
-                "Card Title",
-                textInput("loader_input1", label = "Text input label", width = "100%"),
-                textInput("loader_input2", label = "Text input label", width = "100%"),
-                textAreaInput("loader_input3", label = "Text area input", width = "100%", resize = "none"),
-                actionButton("loader_input4", label = "Action Button", class = "btn btn-primary")
+      class = "loader", id = "loader",
+      div(
+        class = "container-fluid loading-skeleton",
+        style = "margin-top: 76px;",
+        fluidRow(
+          column(
+            width = 10, offset = 1,
+            fluidRow(
+              column(
+                width = 4,
+                card(
+                  "Card Title",
+                  textInput("loader_input1", label = "Text input label", width = "100%"),
+                  textInput("loader_input2", label = "Text input label", width = "100%"),
+                  textAreaInput("loader_input3", label = "Text area input", width = "100%", resize = "none"),
+                  actionButton("loader_input4", label = "Action Button", class = "btn btn-primary")
+                )
+              ),
+              column(
+                width = 8,
+                card("Card Title", img(src = "placeholder.svg", width = "100%"))
               )
-            ),
-            column(
-              width = 8,
-              card("Card Title", img(src = "placeholder.svg", width = "100%"))
             )
           )
         )
